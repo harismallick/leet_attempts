f7be66ef953ce7a9360a86fb2df5e58c3ff83dad
harismallick <mallick.haris@gmail.com>
2024-01-18 01:22:14 +0000

    Leet 100 complete

--- 100_same_tree.py
+++ 100_same_tree.py
@@ -21,27 +21,27 @@ class TreeNode:
 
 def isSameTree(p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
     
+    if p is None and q is None:
+        return True
+    
     if p is not None and q is None:
         return False
     elif q is not None and p is None:
         return False
         
-    if p.val != q.val:
-        return False
-    isSameTree(p.left, q.left)
-    print(p.val, q.val)
-    isSameTree(p.right, q.right)
-
-    return True
+    if p.val == q.val:
+        return isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
+    
+    return False
 
 
 if __name__ == '__main__':
     node1 = TreeNode(1)
     node1.left = TreeNode(2)
-    # node1.right = TreeNode(3)
+    node1.right = TreeNode(3)
     node2 = TreeNode(1)
-    # node2.left = TreeNode(2)
-    node2.right = TreeNode(2)
+    node2.left = TreeNode(2)
+    node2.right = TreeNode(3)
 
     output = isSameTree(node1, node2)
     print(output)
