fd4111c551e753885027561649c265f04e71b03d
harismallick <mallick.haris@gmail.com>
2024-01-26 00:59:14 +0000

    121 complete

--- 121_buy_sell_stock.py
+++ 121_buy_sell_stock.py
@@ -41,10 +41,64 @@ def maxProfit(prices: list[int]) -> int:
 # Solution 2 not accepted by Leet
 # Solution 3:
     
-        
-    pass
+    hashmap: dict = {
+        "stock_low": None,
+        "stock_high": None,
+        "potential_low": None,
+        "high_index": None
+    }
+    for price in prices:
 
+        if hashmap["stock_low"] is None:
+            hashmap["stock_low"] = price
+            continue
+        if hashmap["potential_low"] is not None and price - hashmap["potential_low"] > hashmap["stock_high"] - hashmap["stock_low"]:
+            hashmap["stock_low"] = hashmap["potential_low"]
+            hashmap["stock_high"] = price
+            hashmap["potential_low"] = None
+
+        elif price < hashmap["stock_low"] and hashmap["stock_high"] is None:
+            hashmap["stock_low"] = price
+        elif price < hashmap["stock_low"]:
+            if hashmap["potential_low"] is None: 
+                hashmap["potential_low"] = price
+            elif price < hashmap["potential_low"]:
+                hashmap["potential_low"] = price
+        elif price > hashmap["stock_low"]:
+            if hashmap["stock_high"] is not None:
+                if price >= hashmap["stock_high"]:
+                    hashmap["stock_high"] = price
+                    if hashmap["potential_low"] is not None:
+                        hashmap["stock_low"] = hashmap["potential_low"]
+                        hashmap["potential_low"] = None
+                elif hashmap["potential_low"] is not None and price - hashmap["potential_low"] > hashmap["stock_high"] - hashmap["stock_low"]:
+                    hashmap["stock_low"] = hashmap["potential_low"]
+                    hashmap["stock_high"] = price
+                    hashmap["potential_low"] = None
+            else:
+                hashmap["stock_high"] = price
+
+    if hashmap["stock_high"] is not None and hashmap["stock_low"] is not None:
+        return hashmap["stock_high"] - hashmap["stock_low"]
+    return 0
+# Solution accepted but 2-pointer suggested solution involves much less lines of code:
+
+def maxProfit2(prices):
+    left = 0 #Buy
+    right = 1 #Sell
+    max_profit = 0
+    while right < len(prices):
+        currentProfit = prices[right] - prices[left] #our current Profit
+        if prices[left] < prices[right]:
+            max_profit =max(currentProfit,max_profit)
+        else:
+            left = right
+        right += 1
+    return max_profit
 
 if __name__ == "__main__":
-    print(maxProfit([7,1,5,3,6,4]))
-    print(maxProfit([7,6,4,3,1]))
+    print(maxProfit2([7,1,5,3,6,4]))
+    print(maxProfit2([2,1,2,1,0,1,2]))
+    print(maxProfit2([3,3,5,0,0,3,1,4]))
+    print(maxProfit2([5,6,1,3]))
+
